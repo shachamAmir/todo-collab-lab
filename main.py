d0b02e346a1a32fd61cbc5e9b25bdd4c28bfb7cf
Amir Shacham <shacham.amir@gmail.com>
2026-03-28 12:33:00 +0300

    Student A: update first task

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from todo import add_task, list_tasks
+from todo import add_task, list_tasks, update_first_task
 
 add_task("Initial task by A")
 add_task("B: buy groceries")
 add_task("A: clean room")
 tasks = list_tasks()
+update_first_task("A: updated first task")
 for t in tasks:
     print(t)
