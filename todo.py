2abd27c5aa08af38e9f8a87aaaff1aba0023405c
Amir Shacham <shacham.amir@gmail.com>
2026-03-28 12:30:37 +0300

    Student A: add remove_last_task function

--- todo.py
+++ todo.py
@@ -1,3 +1,11 @@
+def add_task(task):
+    with open("tasks.txt", "a") as f:
+        f.write(task + "\n")
+
+def list_tasks():
+    with open("tasks.txt", "r") as f:
+        return f.readlines()
+
 def remove_last_task():
     with open("tasks.txt", "r") as f:
         lines = f.readlines()
