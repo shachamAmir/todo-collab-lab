1f77264882dd31b13b6fae4239b87f42f6b04473
daniel13ros <Danielros883@gmail.com>
2026-03-28 12:34:40 +0300

    final version of the code , U can execute it

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from todo import add_task, list_tasks, update_first_task
+from todo import add_task, count_tasks, list_tasks, update_first_task
 
 add_task("Initial task by A")
 add_task("B: buy groceries")
@@ -7,3 +7,7 @@ tasks = list_tasks()
 update_first_task("A: updated first task")
 for t in tasks:
     print(t)
+
+add_task("A iteration task")
+add_task("B iteration task")
+print("Total tasks:", count_tasks())
